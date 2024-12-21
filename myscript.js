ad46c2b33d3ca3c8a74a6983bfff981114fcbbd1
malapakavivek <malapakavivek628@gmail.com>
2024-12-22 00:31:03 +0530

    rock paper scissors with js and DOM

--- myscript.js
+++ myscript.js
@@ -8,78 +8,101 @@ let computerChoice;
 
 let humanScore = 0;
 let computerScore = 0;
+let numberOfRounds = 0;
 
-function getComputerChoice(){
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const buttons = document.querySelectorAll("button");
+
+
+function getValue(e){
+    humanChoice = e.target.textContent.toLowerCase()
+    console.log(humanChoice)
     computerChoice = dictionary[Math.floor(Math.random()*3)]
-    return computerChoice
+    console.log(computerChoice)
+    playRound(humanChoice,computerChoice)
+    getScores()
 }
 
-function getHumanChoice(){
-    choice = prompt("Enter your choice");
-    humanChoice = choice.toLowerCase();
-    
-    if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
-        alert("Enter a valid choice");
-    }
-    
-    return humanChoice;
-}
+paper.addEventListener("click",getValue)
+rock.addEventListener("click",getValue)
+scissors.addEventListener("click",getValue)
+
+
+
+const div = document.querySelector("div")
+const para = document.createElement("p")
 
 function playRound(human,computer){
     //Rock
     if (human == "rock" & computer == "paper"){
-        console.log(`You lose ${computer} beats ${human}`);
+        para.textContent =`You lose ${computer} beats ${human}`;
+        div.appendChild(para)
         computerScore+=1;
     }
     if (human == "rock" & computer == "scissors"){
-        console.log(`You win ${human} beats ${computer}`);
+        para.textContent =`You win ${human} beats ${computer}`;
+        div.appendChild(para)
         humanScore+=1;
     }
     if (human == "rock" & computer == "rock"){
-        console.log(`Both chose Rock, It's a draw`);
+        para.textContent =`Both chose Rock, It's a draw`;
+        div.appendChild(para)
     }
     //Paper
     if (human == "paper" & computer == "rock"){
-        console.log(`You win ${human} beats ${computer}`);
+        para.textContent =`You win ${human} beats ${computer}`;
+        div.appendChild(para)
         humanScore+=1;
     }
     if (human == "paper" & computer == "paper"){
-        console.log(`Both chose Paper : It's a draw`);
+        para.textContent =`Both chose Paper, It's a draw`;
+        div.appendChild(para)   
     }
     if (human == "paper" & computer == "scissors"){
-        console.log(`You lose ${computer} beats ${human}`);
+        para.textContent =`You lose ${computer} beats ${human}`;
+        div.appendChild(para)
         computerScore+=1;
     }
     //Scissors
     if (human == "scissors" & computer == "rock"){
-        console.log(`You lose ${computer} beats ${human}`)
+        para.textContent =`You lose ${computer} beats ${human}`;
+        div.appendChild(para)
         computerScore+=1;
     }
     if (human == "scissors" & computer == "paper"){
-        console.log(`You win ${human} beats ${computer}`)
+        para.textContent =`You win ${human} beats ${computer}`;
+        div.appendChild(para)
         humanScore+=1;
     }
     if (human == "scissors" & computer == "scissors"){
-        console.log(`Both chose scissors : it's a draw`)
+        para.textContent =`Both chose scissors, It's a draw`;
+        div.appendChild(para)       }
+
+    numberOfRounds+=1            
+}
+
+function getScores() {
+    const span = document.createElement("span");
+    if (numberOfRounds == 5) {
+        if (humanScore > computerScore) {
+            span.textContent = `You Win \nFinal Scores = You:${humanScore} ComputerScore:${computerScore}`;
+            span.style.whiteSpace = "pre"; // Preserve line breaks
+            div.appendChild(span)
+            endGame();
+        }
+        if (humanScore < computerScore) {
+            span.textContent = `Computer Wins \nFinal Scores = You:${humanScore} ComputerScore:${computerScore}`;
+            span.style.whiteSpace = "pre"; // Preserve line breaks
+            div.appendChild(span)
+            endGame();
+        }
     }
 }
 
-function playGame(){
-    for (i=1;i<=5;i++){
-        let computerSelection = getComputerChoice()
-        let humanSelection = getHumanChoice()
-        playRound(humanSelection,computerSelection);
-        console.log("Your score: ",humanScore + " Computer Score: ",computerScore)
-    }
-    if (humanScore>computerScore){
-        console.log("You Win")
-    }
-    else if (computerScore>humanScore){
-        console.log("Computer Wins")
-    }
-    else{
-        console.log("It's a draw")
-    }
+
+function endGame(){
+    buttons.forEach((button) => button.setAttribute("disabled", "true"));
 }
 
-playGame();
